6407a974ab19b12c90d1e348c7bb0482df3a393b
Anastasiia <vostrova_1994@mail.ru>
2022-03-01 16:14:07 +0300

    HW3-11

---
+++ HW3-11/HW3-11/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Write("Введите целое положительное  число (N): ");
+int N = Convert.ToInt32(Console.ReadLine());
+int y;
+for (int i = 1; i < N; i++)
+{
+    int summPositive = 0;
+    int summNegative = 0;
+    int x=i;
+    while (x != 0)
+    {
+        y = x % 10;
+        if (y % 2 == 0)
+        {
+            summPositive = summPositive + y;
+        }
+        else
+        {
+            summNegative = summNegative + y;
+        }
+        x = x / 10;
+
+    } 
+    if (summPositive > summNegative)
+    {
+        Console.Write(i +" ");
+    }
+}
+
+ 
