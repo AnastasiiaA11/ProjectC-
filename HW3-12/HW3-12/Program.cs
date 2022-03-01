df3903515e41e27fd5f948c30b8e2ccc0eae6a02
Anastasiia <vostrova_1994@mail.ru>
2022-03-01 12:51:34 +0300

    finished assignments

--- HW3-12/HW3-12/Program.cs
+++ HW3-12/HW3-12/Program.cs
@@ -4,17 +4,31 @@ Console.WriteLine("Введите первое число");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int B = Convert.ToInt32(Console.ReadLine());
-int tmp;
-int z;
-int z2;
-while (A!=0)
-{
-    z = A % 10;
-    while (B!=0)
-    {
-        z2 = B % 10;
-        if (z==z2)
+bool check = false;
 
-        
+while (A != 0)
+{
+    int tmp = A % 10;
+    while (B != 0)
+    {
+        int tMppp = B % 10;
+        if (tmp == tMppp) 
+        {
+            check = true;
+        }
+        B = B / 10;
     }
+    A = A / 10;
 }
+    if (check == true)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+
+
+
+
---
+++ HW3-8/HW3-8/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.WriteLine("Введите число куба числа N");
+int enteredNumber = Convert.ToInt32(Console.ReadLine());
+
+int middle = 0;
+int numberRight = 0;
+int numberLeft = enteredNumber;
+
+do
+{
+    middle = (numberRight + numberLeft) / 2;
+    if (Math.Pow(middle, 3) < middle)
+    {
+        numberRight = middle;
+    }
+    else
+    {
+        numberLeft = middle;
+    }
+}
+while (Math.Pow(middle, 3) != enteredNumber);
+Console.WriteLine(middle);
+
+
